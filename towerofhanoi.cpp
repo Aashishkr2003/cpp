f25de78a08298535bb941e5a4421784459d0bcfa
Aashishkr2003 <115491871+Aashishkr2003@users.noreply.github.com>
2022-10-17 13:38:58 +0530

    Create towerofhanoi.cpp

---
+++ towerofhanoi.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void towerOfHanoi(int n, char source, char dest, char auxi)
+{
+    if (n == 0) {
+        return;
+    }
+    towerOfHanoi(n - 1, source, auxi, dest);
+    cout << "Move disk " << n << " source " << source
+         << " dest " << dest<< endl;
+    towerOfHanoi(n - 1, auxi, dest, source);
+}
+int main()
+{
+    int N = 3;
+    towerOfHanoi(N, 'A', 'C', 'B');
+    return 0;
+}
